d25be32c0d7249f2f909c9fd50618389d0594294
Erwan THOUVENOT <erwanthouvenot@gmail.com>
2012-05-20 16:57:20 +0200

    Test

--- Avatac/src/avatac/Avatac.java
+++ Avatac/src/avatac/Avatac.java
@@ -14,6 +14,6 @@ public class Avatac {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        test
     }
 }
